c8816e5ce95b5454a34e5dd11642b49109b5d8e5
samzri1 <109951209+samzri1@users.noreply.github.com>
2023-06-08 14:31:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,58 +16,75 @@ provider "azurerm" {
    features {}
 }
 
-resource "azurerm_resource_group" "rsgrp" {
-  name     = "testjenkinsTerra"
+# Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "example-Jenkins-terra"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "avn" {
-  name                = "jknetwork"
+provider "azurerm" {
+   subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+   #client_id = "1244ff47-5233-442c-b8df-b761e220bc23"
+   #client_secret = "YtY8Q~Q6kLoKDGGGEn3lGlILr--HZ5EsbJwOGba-"
+   tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
+   skip_provider_registration = true
+   features {}
+}
+# Virtual Network
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.jktfrg.location
-  resource_group_name = azurerm_resource_group.jktfrg.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "azsubnet" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.jktfrg.name
-  virtual_network_name = azurerm_virtual_network.jktfvm.name
-  address_prefixes     = ["10.0.2.0/24"]
+# Subnet
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "azni" {
+# Network Interface
+resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  location            = azurerm_resource_group.jktfrg.location
-  resource_group_name = azurerm_resource_group.jktfrg.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.jktfsub.id
+    name                          = "example-ipconfig"
+    subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "azvm" {
-  name                = "Vmjktf"
-  resource_group_name = azurerm_resource_group.jktfrg.name
-  location            = azurerm_resource_group.jktfrg.location
-  size                = "Standard_DS1_v2"
-  admin_username      = "adminuser"
-  admin_password      = "@Azurev69007"
-  disable_password_authentication = "false"
-  network_interface_ids = [
-    azurerm_network_interface.jktfnic.id,
-  ]
+# Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-vm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  size                 = "Standard_DS1_v2"
+  admin_username       = "adminuser"
+  network_interface_ids = [azurerm_network_interface.example.id]
 
   os_disk {
-    caching              = "ReadWrite"
+    name              = "example-osdisk"
+    caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
+  }
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDY8sIrQZ/w5Q4N4W0ed8lqEg5B+p+4tkDM7bdg1MwvWmfnrArKAGWKnw+oxJon008YXGq/7fynatuQ52RyZUUnGC9rk5IIL6/+Cld75ex17P01wPAf73baI4/ZiqXXh36dMqlDsacPgrBJOZJ6ml1FAkFOi1mmVVgpENJjOzH27eyQNdRw/0OY5H98b12vdVqBqIN+shc5aIeAvMgPIHHo7S+UfQtSdOrfQ8xcTzbNLnRUJjB7T3r/I34b7eKfcQSiwbf/UaBa7YhQX67I+0oFDID5E7MJQcnuo1Ji1hqWiKOkhi3evXiOwEWIxOf/5XQ7auQr0FQ83wkHKH4K75i0C9kxZRw+bZ3pzL0fmKGQ5P2ii88+6izMTD/GK+xEn+ydGBtMEqMmQSbBQH+HOiJA44YU8MPtv3mHTK6ocqb1pmf5k/JDKpU+xPLeZUrqOV2sH/1yoCdT+TltTTWPdlj5LF3T0upi301ryy0yPVjfc7XE9Frg3Oxo8y+LW5GXl/sPmPUVR7ffU5ByZz8ruUElPyTt1F7Q439T8p1C6dky7T8llebS8ACsSUrA/0V112OEhhkC9NcwJHo7d8TgM6f0Zus3eYJ9A5/Hz58CX4/0IJw5rVxJbUC7OBQql5SbLo8qZ3Of4jtJKJ9Lp1wxZXd8GaonbCeE1tjhiqrjKvwGPw== azureuser@myVM"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
+
